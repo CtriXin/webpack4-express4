d09ec13e465e60160778dbf1b2534531ccd549f4
song xin <xin.song@funplus.com>
2018-07-09 18:16:34 +0800

    路由

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,12 @@ var app = express();
 var router = express.Router();
 
 var myLogger = function (req, res, next) {
-  console.log('LOGGED lol');
+  console.log('router - home - logger');
   next();
 };
 
-// router.use(myLogger)
-
 /* GET home page. */
 router.get('/', myLogger, function (req, res, next) {
-  // console.log(req.query.token, req.query.uid);
   console.log('用戶端語系：' + req.acceptsLanguages()[0], req.language);
   res.render('index', {
     // title: req.t('lang'),
