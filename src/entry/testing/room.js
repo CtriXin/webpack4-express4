ea10b93a23d84bfd47e3c573bcdb94e100ff667f
song xin <draftdog@foxmail.com>
2018-07-24 18:13:41 +0800

    更新

--- src/entry/testing/room.js
+++ src/entry/testing/room.js
@@ -2,208 +2,184 @@
  * @Author: xin.song 
  * @Date: 2018-07-04 17:39:03 
  * @Last Modified by: xin.song
- * @Last Modified time: 2018-07-23 15:21:44
+ * @Last Modified time: 2018-07-24 18:13:31
  */
-import Vue from 'vue';
+import Vue from "vue";
 
 // import axios from 'axios'
 
-import io from 'socket.io-client'
+import io from "socket.io-client";
 
-
-import '../../css/testChat.scss'
+import "../../css/testChatRoom.scss";
 
 var socket = io();
 
 let indexPage = {
     el: "#vm",
     data: {
-        myname: '',
-        username: '',
-        addName: true,
-        msg: '',
-        totalNum: '',
-        avatar: '',
-        selectAvatar: '',
-        baseTime: '',
-        lastMsgTime: 1,
-        thisMsgTime: 1,
-        room: $("#roomid").text().trim("&nbsp;"),
+        usernameInput: "", //用户输入的名字
+        avatarInput: "", //用户输入的头像
+        selectAvatar: "", //选择的头像
+        addNewUser: true, //增加用户
+        isOldUser: false, //老用户
+        userInfo: "", //用户数据集=
+        totalNum: "", //总用户数
+        userList: "" //在线用户组
     },
     created: function () {
         let self = this;
-
-        
-
-        socket.emit('join', {
-            room: self.room,
-            public: true, //TODO:将来加上判断公共还是私有的房间
-        });
-
-
-        // let myname;
-        // let myavatar;
-        // self.baseTime = new Date().toLocaleString();
-        // if (myname != null && myavatar != null) {
-        //     console.log('用户已登录');
-        //     self.username = myname;
-        //     self.avatar = myavatar;
-        //     self.addName = false;
-
-        //     socket.emit('add user', {
-        //         username: myname,
-        //         avatar: myavatar
-        //     });
-        // }
-        self.lastMsgTime = Math.round(new Date().getTime() / 1000)
-
+        //检查数据
+        socket.emit("checkRoom");
+        socket.emit("checkUser");
+        socket.emit("checkTotalNum");
+        let data = sessionStorage.getItem("userInfo");
+        let lastTime = sessionStorage.getItem("userInfoAddTime");
+        let thisTime = Math.round(new Date().getTime() / 1000);
+        console.log(data, thisTime - lastTime);
+        if (thisTime - lastTime > 60 * 60) {
+            console.log("session保存超过一小时，强制退出");
+            sessionStorage.clear();
+            return;
+        }
+        if (data) {
+            self.addNewUser = false;
+            self.userInfo = JSON.parse(data);
+            socket.emit("loadInfo", self.userInfo);
+            self.isOldUser = JSON.parse(data).oldUser;
+        }
     },
     mounted: function () {
         let self = this;
-        // 加入房间
-        socket.on('publishMessageRoom', function (data) {
-            self.thisMsgTime = Math.round(new Date().getTime() / 1000)
-            console.log(self.thisMsgTime - self.lastMsgTime);
-            let nowtime = new Date().toLocaleString();
-            let time
-            if (self.thisMsgTime - self.lastMsgTime > 60) {
-                time = '<div class="time-keepme">' + nowtime + '</div>'
-            }
-            $('#memo').append(time);
 
-            let html
-            if (data.uname === self.username) {
-                html = '<div class="my-message-keepme"><div class="my-avatar"><img src="' + data.avatar + '" alt=""></div><div class="my-message-txt">' + data.msg + '</div></div>'
-            } else {
-                html = '<div class="fri-message-keepme"><div class="fri-avatar"><img src="' + data.avatar + '" alt=""></div><div class="fri-message-group"><div class="fri-name">' + data.uname + '</div><div class="fri-message-txt">' + data.msg + '</div></div></div>'
-            }
-            $('#memo').append(html);
-            window.scrollTo(0, document.body.scrollHeight)
+        //注册/登录成功
+        socket.on("signInSuccess", function (data) {
+            console.log(data);
+            self.addNewUser = false; //关闭注册
+            self.userInfo = data.userInfo;
+            self.isOldUser = data.oldUser;
+            sessionStorage.setItem("userInfo", JSON.stringify(data.userInfo));
+            let lastMsgTime = Math.round(new Date().getTime() / 1000);
+            sessionStorage.setItem("userInfoAddTime", lastMsgTime);
+        });
+        socket.on("loginSuccess", function (data) {
+            console.log(data);
+            self.addNewUser = false; //关闭注册
+            self.userInfo = data.userInfo;
+            self.isOldUser = data.oldUser;
+            self.newsid = data.sid;
+            sessionStorage.setItem("userInfo", JSON.stringify(data.userInfo));
+            let lastMsgTime = Math.round(new Date().getTime() / 1000);
+            sessionStorage.setItem("userInfoAddTime", lastMsgTime);
         });
 
-
-
-        //登录成功
-        socket.on('loginSuccessRoom', function (data) {
-            self.username = data.username
-            self.avatar = data.avatar
-            self.addName = false;
-            sessionStorage.setItem('myname', data.username);
-            sessionStorage.setItem('myavatar', data.avatar);
-            sessionStorage.setItem('myroom', data.room);
-            return false;
+        //显示房间列表
+        socket.on("showRoom", function (data) {
+            self.roomlist = data;
         });
 
-
-
-        //又登陆了
-        socket.on('loginAgainRoom', function (user) {
-            $('#memo').append($('<div class="memo-info">').text('用户 ' + user + ' 又回来了'));
+        //显示在线用户
+        socket.on("showUser", function (data) {
+            console.log("用户信息", data);
+            self.userList = data;
+            console.log(self.userList);
         });
 
-        /*退出群聊提示*/
-        socket.on('leaveRoom', function (name) {
-            console.log(name, self.username);
-            if (name != null && name == self.username) {
-                $('#memo').append($('<div class="memo-info">').text('用户 ' + name + ' 已退出'));
-                $('.avatar-img').removeClass('avatar-img-keepme');
-                $('.correct-icon').hide();
-                self.addName = true
-            } else {
-                $('#memo').append($('<div class="memo-info">').text('用户 ' + name + ' 已退出'));
-            }
-        })
-
-
-        // 登录成功
-        socket.on('signSuccessRoom', function (user) {
-            $('#memo').append($('<div class="memo-info">').text('用户 ' + user + ' 进入房间了'));
-        });
-
-
-        //计数
-        socket.on('countNumRoom', function (num) {
+        //显示在线用户数
+        socket.on("showTotalNum", function (num) {
             self.totalNum = num;
+            console.log(self.totalNum);
         });
 
+        // 开始聊天
+        socket.on("startChat", function (data) {
+            console.log(
+                data,
+                "http://localhost:5657/test/private/" +
+                data.fromuid +
+                "-" +
+                data.touid
+            );
+        });
+
+        //更新sid
+        socket.on("updateid", function (data) {
+            console.log(data);
+            self.userInfo.sid = data.sid;
+            self.userInfo.uid = data.uid;
+            let userinfo = JSON.parse(sessionStorage.getItem("userInfo"));
+            userinfo.sid = data.sid;
+            userinfo.uid = data.uid;
+            sessionStorage.setItem("userInfo", JSON.stringify(userinfo));
+        });
+
+        //历史消息
+        socket.on("historyMsg", function (data) {
+            console.log(data);
+            console.log(self.userList);
+            for (var i in self.userList) {
+                if (self.userList[i].uid == data.fromuid) {
+                    self.userList[i].haveMsg = true;
+                    self.userList[i].room = data.room;
+                }
+            }
+        });
     },
     methods: {
         adduser() {
             let self = this;
-            self.username = $.trim($('#add-user-input').val());
-            console.log(self.username, self.avatar);
-            if (self.username && self.avatar != "") {
-                socket.emit('newUserRoom', {
-                    username: self.username,
-                    avatar: self.avatar,
-                    room: self.room
+            self.usernameInput = $.trim($("#add-user-input").val());
+            console.log(self.usernameInput, self.avatarInput);
+            if (self.usernameInput && self.avatarInput != "") {
+                socket.emit("signin", {
+                    username: self.usernameInput,
+                    avatar: self.avatarInput
                 });
-                $('#add-user-input').val('');
-
+                $("#add-user-input").val("");
             } else {
-                alert('请输入用户名或选择头像')
+                alert("请输入用户名或选择头像");
             }
-
         },
-        sendMsg() {
+        chat(uid, name) {
             let self = this;
-            self.msg = $('#msg').val();
-            if (self.msg == '') {
-                alert('请不要输入空消息');
-                return
-            }
-            socket.emit('postMessageRoom', {
-                room: self.room,
-                uname: self.username,
-                avatar: self.avatar,
-                msg: self.msg
+            console.log("touid " + uid);
+            console.log(
+                "http://localhost:5657/test/private/" +
+                self.userInfo.uid +
+                "-" +
+                uid
+            );
+            socket.emit("gochat", {
+                uid: uid,
+                username: name
             });
-            $('#msg').val('');
-            return false;
         },
         chooseAvatar(num, event) {
             let self = this;
             let el = event.target;
-            $('.avatar-img').addClass('avatar-img-keepme');
-            $(el).removeClass('avatar-img-keepme')
+            $(".avatar-img").addClass("avatar-img-keepme");
+            $(el).removeClass("avatar-img-keepme");
             console.log(el, event);
-            console.log($(el).attr('src'));
-            self.avatar = $(el).attr('src');
+            console.log($(el).attr("src"));
+            self.avatarInput = $(el).attr("src");
             self.selectAvatar = num;
-        },
-        logout() {
-            console.log('hahaha');
-            let self = this;
-            sessionStorage.clear();
-            socket.emit('logoutRoom', {
-                username: self.username,
-                room: self.room
-            });
-        },
-
+        }
         //FIXME: END HERE
     }
 };
 
-
-
-
 $(() => {
-
     // Start
     let VM = new Vue(indexPage);
     console.log(VM);
 
-
-    $('#msg').keydown(function (event) {
+    $("#msg").keydown(function (event) {
         if (event.keyCode == 13) {
-            VM.sendMsg()
+            VM.sendMsg();
         }
-    })
-    $('#add-user-input').keydown(function (event) {
+    });
+    $("#add-user-input").keydown(function (event) {
         if (event.keyCode == 13) {
-            VM.adduser()
+            VM.adduser();
         }
-    })
-
+    });
 });
