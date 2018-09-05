eccc7a47bc9510a2466ffc277b0bdd81967f2fa5
song xin <draftdog@foxmail.com>
2018-09-05 15:51:01 +0800

    更新

--- routes/test.js
+++ routes/test.js
@@ -8,10 +8,33 @@ router.get('/', function (req, res, next) {
   });
 });
 
+router.get('/im', function (req, res, next) {
+  res.render('testing/im', {
+    title: 'IM 测试'
+  });
+});
+
 
 router.get('/chat', function (req, res, next) {
   res.render('testing/chat', {
     title: '测试socket.io'
+  });
+});
+router.get('/room', function (req, res, next) {
+  res.render('testing/chat_room', {
+    title: '测试socket.io'
+  });
+});
+router.get('/private/:from-:with', function (req, res, next) {
+  let user1 = req.params.from
+  let user2 = req.params.with
+  console.log(user1, user2);
+  res.render('testing/private', {
+    title: '测试socket.io',
+    user1: user1,
+    user2: user2,
+    roomid: user1 + '-' + user2
+
   });
 });
 router.get('/chat/:roomID', function (req, res, next) {
@@ -21,7 +44,6 @@ router.get('/chat/:roomID', function (req, res, next) {
     roomid: roomID
   });
 });
-
 
 
 
